de02cd63f4f58fb0e6db7f6faad09304e36c0f69
Worlasi15 <efyawalasi@gmail.com>
2024-02-15 17:11:04 -0800

    lockboxes

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,33 +1,28 @@
 #!/usr/bin/python3
+"""
+a method that determines if all the boxes can be opened
+"""
+
+
 def canUnlockAll(boxes):
-    if not boxes or len(boxes) == 0:
-        return False
-
     # Initialize a set to keep track of opened boxes
-    opened_boxes = set([0])
+    opened_boxes = set()
+    opened_boxes.add(0)  # Start with the first box unlocked
 
-    # Initialize a list to keep track of keys that need to be checked
-    keys_to_check = boxes[0]
+    # Initialize a set to keep track of keys
+    keys = set(boxes[0])
 
-    # Iterate through the keys to check
-    while keys_to_check:
-        current_key = keys_to_check.pop()
+    # Iterate through the keys and boxes until no new keys or boxes are found
+    while keys:
+        # Get a key from the set of keys
+        key = keys.pop()
 
-        # Check if the key opens a new box
-        if current_key < len(boxes) and current_key not in opened_boxes:
-            opened_boxes.add(current_key)
-            keys_to_check.extend(boxes[current_key])
+        # If the key corresponds to a box that hasn't been opened yet
+        if key < len(boxes) and key not in opened_boxes:
+            # Add the box to the set of opened boxes
+            opened_boxes.add(key)
+            # Add any new keys found in the opened box to the set of keys
+            keys.update(boxes[key])
 
-    # Check if all boxes are opened
+    # If all boxes are opened, return True; otherwise, return False
     return len(opened_boxes) == len(boxes)
-
-
-# Example usage:
-boxes1 = [[1], [2], [3], [4], []]
-print(canUnlockAll(boxes1))  # Output: True
-
-boxes2 = [[1, 4, 6], [2], [0, 4, 1], [5, 6, 2], [3], [4, 1], [6]]
-print(canUnlockAll(boxes2))  # Output: True
-
-boxes3 = [[1, 4], [2], [0, 4, 1], [3], [], [4, 1], [5, 6]]
-print(canUnlockAll(boxes3))  # Output: False
