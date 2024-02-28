3fd360f30262f83a96f1ec2a033294c26cd31a58
Worlasi15 <efyawalasi@gmail.com>
2024-02-28 14:21:25 -0800

    log parsing

---
+++ 0x03-log_parsing/0-stats.py
@@ -0,0 +1,40 @@
+mport sys
+from collections import defaultdict
+import signal
+
+def print_statistics(total_size, status_counts):
+    print(f'Total file size: File size: {total_size}')
+    for status_code in sorted(status_counts.keys()):
+        print(f'{status_code}: {status_counts[status_code]}')
+
+def signal_handler(sig, frame):
+    print_statistics(total_size, status_counts)
+    sys.exit(0)
+
+signal.signal(signal.SIGINT, signal_handler)
+
+line_count = 0
+total_size = 0
+status_counts = defaultdict(int)
+
+try:
+    for line in sys.stdin:
+        line_count += 1
+
+        try:
+            _, _, _, _, _, status_code, file_size = line.strip().split()
+            status_code = int(status_code)
+            file_size = int(file_size)
+        except ValueError:
+            continue
+
+        total_size += file_size
+        status_counts[status_code] += 1
+
+        if line_count % 10 == 0:
+            print_statistics(total_size, status_counts)
+
+except KeyboardInterrupt:
+    pass
+
+print_statistics(total_size, status_counts)
