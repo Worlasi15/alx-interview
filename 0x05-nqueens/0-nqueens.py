aa6212b9dc482fce355fb67ed8d2a6370be400ce
Worlasi15 <efyawalasi@gmail.com>
2024-03-17 17:20:42 -0700

    puzzle for the queens

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -2,17 +2,14 @@
 import sys
 
 def is_safe(board, row, col, N):
-    # Check this row on the left side
     for i in range(col):
         if board[row][i] == 1:
             return False
     
-    # Check upper diagonal on left side
     for i, j in zip(range(row, -1, -1), range(col, -1, -1)):
         if board[i][j] == 1:
             return False
     
-    # Check lower diagonal on left side
     for i, j in zip(range(row, N), range(col, -1, -1)):
         if board[i][j] == 1:
             return False
@@ -20,25 +17,22 @@ def is_safe(board, row, col, N):
     return True
 
 def solve_n_queens_util(board, col, N, result):
-    # If all queens are placed then return true
     if col == N:
         result.append([[row, col] for row in range(N) if board[row][col] == 1])
         return True
     
     res = False
     for i in range(N):
-        # Check if queen can be placed on board[i][col]
         if is_safe(board, i, col, N):
-            # Place this queen in board[i][col]
             board[i][col] = 1
-            
-            # recur to place rest of the queens
             res = solve_n_queens_util(board, col + 1, N, result) or res
-            
-            # If placing queen in board[i][col] doesn't lead to a solution, then remove queen from board[i][col]
             board[i][col] = 0
     
     return res
+
+def print_solution(result):
+    for sol in result:
+        print(sol)
 
 def solve_n_queens(N):
     if not isinstance(N, int):
@@ -51,8 +45,7 @@ def solve_n_queens(N):
     board = [[0 for _ in range(N)] for _ in range(N)]
     result = []
     solve_n_queens_util(board, 0, N, result)
-    for solution in result:
-        print(solution)
+    print_solution(result)
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
