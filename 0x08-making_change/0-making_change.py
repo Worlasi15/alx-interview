ef85582cd7e665fe6c425c621415397e81b9e076
Worlasi15 <efyawalasi@gmail.com>
2024-04-04 17:53:04 -0700

    change

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,33 +1,23 @@
 #!/usr/bin/python3
-"""
-Module for making change problem
-"""
+"""A function to determine the fewest number of coins needed
+   to meet a given amount total"""
+
 
 def makeChange(coins, total):
+    """This function will take a list of coins and use
+       that to calculate how much change the total will require
     """
-    Determines the fewest number of coins needed to meet a given amount total
-    """
-    if total < 1:
+    if total <= 0:
         return 0
 
-    # Initialize a list to store the fewest number of coins needed for each value from 0 to total
-    dp = [float('inf')] * (total + 1)
-    dp[0] = 0  # 0 coins needed for total of 0
-
-    # Iterate through each coin value
-    for coin in coins:
-        # For each coin value, update the dp list to store the fewest number of coins needed
-        for i in range(coin, total + 1):
-            dp[i] = min(dp[i], dp[i - coin] + 1)
-
-    # If dp[total] is still float('inf'), it means the total cannot be met by any number of coins
-    return dp[total] if dp[total] != float('inf') else -1
-
-if __name__ == "__main__":
-    coins = [1, 2, 25]
-    total = 37
-    print(makeChange(coins, total))  # Output should be 7
-
-    coins = [1256, 54, 48, 16, 102]
-    total = 1453
-    print(makeChange(coins, total))  # Output should be -1
+    else:
+        coin = sorted(coins)
+        coin.reverse()
+        counter = 0
+        for i in coin:
+            while(total >= i):
+                counter += 1
+                total -= i
+        if total == 0:
+            return counter
+        return -1
